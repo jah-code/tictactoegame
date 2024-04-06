d1dee68d46c9b4c6e579adfe54c410366ae24e57
lijah <lijah@mansol.net.au>
2024-04-06 22:38:35 +0800

    initial interface

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 
 function App() {
+  const Square = (props) => {
+    return <button className="square">X</button>;
+  };
+
   return (
     <div>
-      <h1>Tic Tic Toe</h1>
+      <h1>Tic Tac Toe </h1>
+      <div className="board">
+        {Array(9)
+          .fill(null)
+          .map((e, i) => {
+            return <Square key={i} />;
+          })}
+      </div>
     </div>
   );
 }
