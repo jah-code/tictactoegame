928de709418056d6caaffd89c87467c59b4bb692
lijah <lijah@mansol.net.au>
2024-04-07 14:43:57 +0800

    With the value appear on click of each square

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [moves, setMoves] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentMoveArr = moves[currentMove];
+
   const Square = (props) => {
-    return <button className="square">X</button>;
+    const { val, onClickPlayHandler } = props;
+    return (
+      <button className="square" onClick={onClickPlayHandler}>
+        <span className="value">{val}</span>
+      </button>
+    );
+  };
+
+  const Board = (props) => {
+    const { currentMove, currentMoveArr } = props;
+
+    const onClickPlayHandler = (i) => {
+      const newMoveArr = currentMoveArr;
+      if (currentMove % 2 === 0) {
+        newMoveArr[i] = "X";
+      } else {
+        newMoveArr[i] = "O";
+      }
+
+      const newMoves = [...moves, newMoveArr];
+      setMoves(newMoves);
+      setCurrentMove(newMoves.length - 1);
+    };
+
+    return (
+      <div className="board">
+        {currentMoveArr.map((e, i) => {
+          return (
+            <Square
+              key={i}
+              val={e}
+              onClickPlayHandler={() => onClickPlayHandler(i)}
+            />
+          );
+        })}
+      </div>
+    );
   };
 
   return (
     <div>
       <h1>Tic Tac Toe </h1>
-      <div className="board">
-        {Array(9)
-          .fill(null)
-          .map((e, i) => {
-            return <Square key={i} />;
-          })}
-      </div>
+      <Board currentMove={currentMove} currentMoveArr={currentMoveArr} />
     </div>
   );
 }
