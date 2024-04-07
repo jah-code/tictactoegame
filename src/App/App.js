a5245dcf1eef80117df10f0d2e181107dfcf31cf
lijah <lijah@mansol.net.au>
2024-04-07 15:19:50 +0800

    Added the winner combination code

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [moves, setMoves] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [status, setStatus] = useState("");
 
   const currentMoveArr = moves[currentMove];
 
@@ -32,27 +33,61 @@ function App() {
       setCurrentMove(newMoves.length - 1);
     };
 
+    const winner = winnerCombinations(currentMoveArr);
+    if (winner) {
+      setStatus("Winner is " + winner);
+    } else {
+      setStatus("Next player is " + (currentMove % 2 === 0 ? "X" : "O"));
+    }
+
     return (
-      <div className="board">
-        {currentMoveArr.map((e, i) => {
-          return (
-            <Square
-              key={i}
-              val={e}
-              onClickPlayHandler={() => onClickPlayHandler(i)}
-            />
-          );
-        })}
+      <div>
+        <div className="board">
+          {currentMoveArr.map((e, i) => {
+            return (
+              <Square
+                key={i}
+                val={e}
+                onClickPlayHandler={() => onClickPlayHandler(i)}
+              />
+            );
+          })}
+        </div>
+        <h4 className="status">{currentMove > 0 && status}</h4>
       </div>
     );
   };
 
   return (
     <div>
-      <h1>Tic Tac Toe </h1>
+      <h1 className="title">Tic Tac Toe </h1>
       <Board currentMove={currentMove} currentMoveArr={currentMoveArr} />
     </div>
   );
+}
+
+function winnerCombinations(currentMoveArr) {
+  const combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let x = 0; x < combinations.length; x++) {
+    const [a, b, c] = combinations[x];
+    if (
+      currentMoveArr[a] &&
+      currentMoveArr[a] === currentMoveArr[b] &&
+      currentMoveArr[a] === currentMoveArr[c]
+    ) {
+      return currentMoveArr[a];
+    }
+  }
+  return null;
 }
 
 export default App;
